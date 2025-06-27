3b6f5c90feded927317a88fbb3ad4bbc2b32017d
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2025-06-27 22:37:38 +0700

    ✨feature: add: dashboard

---
+++ src/pages/Admin/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LayoutAdmin from '../../../layouts/admin/LayoutAdmin/LayoutAdmin'
+
+const Dashboard = () => {
+  return (
+    <LayoutAdmin>
+        <div className='admin-dashboard'>Dashboard</div>
+    </LayoutAdmin>
+  )
+}
+
+export default Dashboard
