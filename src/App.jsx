cb01619625b29e06257c7f724be89a28b2831bbe
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2025-06-27 20:52:22 +0700

    ✨feature: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import { useRoutes, Navigate } from "react-router-dom";
 import "./App.scss";
+import Dashboard from "./pages/Admin/dashboard/Dashboard";
+import Members from "./pages/Admin/Members/Members";
+import MemberForm from "./components/admin/member/MemberForm";
+
 
 function App() {
+  const elements = useRoutes([
+    {
+      path: "/",
+      element: <Navigate to="/admin/dashboard" />
+    },
+    {
+      path: "/admin/dashboard",
+      element: <Dashboard/>
+    },
+    {
+      path: "/admin/members",
+      element: <Members/>
+    },
+    {
+      path: "/admin/members/create",
+      element: <MemberForm modal="add"/>
+    },
+    {
+      path: "/admin/members/edit/:id",
+      element: <MemberForm modal="edit"/>
+    }
+
+  ])
   return (
     <>
-      <h1>Hello World</h1>
+      {elements}
+      {/* <DeleteMember/> */}
     </>
   );
 }
