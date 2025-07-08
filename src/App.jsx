f2bd9357547c9c2a3c5a2f8fa94335071b77562f
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2025-07-08 10:59:51 +0700

    🔵 other: commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, useRoutes } from 'react-router-dom'
 import ForgotPasswordPage from './pages/ForgotPasswordPage/ForgotPasswordPage'
 import MainLayout from './layouts/mainLayout/mainLayout'
 import UserHomePage from './pages/userHomePage/userHomePage'
+import ProfilePage from './pages/ProfilePage/ProfilePage'
 
 function App() {
   let elements = useRoutes([
@@ -20,14 +21,18 @@ function App() {
       element: <ForgotPasswordPage />,
     },
     {
-      path: '/home',
+      path: '/user',
       element: <MainLayout />,
       children: [
         {
-          path: '',
+          path: 'home',
           element: <UserHomePage />,
           index: true,
         },
+        {
+          path: 'profile',
+          element: <ProfilePage/>,
+        }
       ],
     },
   ])
