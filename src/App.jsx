d75c56965e2c164b985827af3419c093f39c6e8c
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2025-07-26 17:36:58 +0700

    ✨feature: add library hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Events from './pages/Admin/events/Events'
 import EventForm from './components/admin/event/EventForm'
 import LayoutAdmin from './layouts/LayoutAdmin/LayoutAdmin'
 import ProfilePage from './pages/ProfilePage/ProfilePage'
+import { Toaster } from 'react-hot-toast';
 function App() {
   const elements = useRoutes([
     {
@@ -75,6 +76,11 @@ function App() {
       ],
     },
   ])
-  return <>{elements}</>
+  return (
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      {elements}
+    </>
+  );
 }
 export default App
